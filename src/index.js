e805765529f07384cb951aa458c3fbc898422ab0
srwatlanta <shawnrwheeler@gmail.com>
2019-09-04 16:56:59 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+handleClick = () => {
+
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={console.log("whatsup")} />
   </div>,
   document.getElementById('root')
 );
